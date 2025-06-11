9b06d5b10c2bea5c182d301c5206f72e90d60987
Umair Khan <uk236393@gmail.com>
2025-06-11 16:39:00 +0500

    ProductFilterScreen

--- app/src/main/java/com/example/newprojects/FilterList.kt
+++ app/src/main/java/com/example/newprojects/FilterList.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -18,7 +20,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FilterListExample() {
@@ -53,4 +57,54 @@ fun FilterListExample() {
             )
         }
     }
+}
+
+
+@Composable
+fun FilteredListScreen() {
+
+    var searchGrouped by remember { mutableStateOf("") }
+
+    val allFruits = listOf(
+        "Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blueberry",
+        "Cherry", "Coconut", "Cranberry", "Date", "Dragonfruit", "Durian",
+        "Fig", "Grape", "Guava", "Kiwi", "Lemon", "Lime", "Mango", "Melon",
+        "Nectarine", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Plum",
+        "Pomegranate", "Raspberry", "Strawberry", "Watermelon"
+    )
+
+    val groupedFruits = allFruits.filter { it.contains(searchGrouped, ignoreCase = true) }
+        .groupBy { it.first().uppercaseChar() }
+
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            TextField(value = searchGrouped, onValueChange = {
+                searchGrouped = it
+            })
+        }
+        groupedFruits.forEach { (letter, list) ->
+            item {
+                Text(
+                    text = letter.toString(),
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.DarkGray
+                )
+            }
+
+            items(list) { fruit ->
+                Text(text = fruit, fontSize = 8.sp, color = Color.Green)
+
+            }
+
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/newprojects/MainActivity.kt
+++ app/src/main/java/com/example/newprojects/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.newprojects.ui.theme.NewprojectsTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,4 +42,72 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+data class Product(val name: String, val category: String)
+
+@Composable
+fun ProductFilterScreen() {
+    var selectedCategory by remember { mutableStateOf("All") }
+
+    val productList = listOf(
+        Product("Burger", "Food"),
+        Product("Pizza", "Food"),
+        Product("Laptop", "Electronics"),
+        Product("T-Shirt", "Clothing"),
+        Product("Headphones", "Electronics"),
+        Product("Jeans", "Clothing")
+    )
+
+
+    val categories = listOf("All", "Food", "Electronics", "Clothing")
+
+    val filteredList = remember(selectedCategory) {
+        if (selectedCategory == "All") {
+            productList
+        } else {
+            productList.filter { it.category == selectedCategory }
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState())
+        ) {
+            categories.forEach { categories ->
+                Button(
+                    onClick = { selectedCategory = categories },
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = if (selectedCategory == categories) Color.Gray else Color.LightGray
+                    )
+                ) {
+                    Text(text = categories)
+
+                }
+
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn {
+            filteredList.forEach { product ->
+                item {
+                    Text(
+                        text = "${product.name} (${product.category})",
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+    }
+
 }
